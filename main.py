d0f51bea6444542acf9eb3380dbd1a222b6be26e
Devansh3712 <devanshamity@gmail.com>
2022-04-18 22:47:41 +0530

    Validate URL

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -33,6 +35,19 @@ async def home(request: Request):
 @app.post("/", response_class=HTMLResponse)
 async def short_url(request: Request):
     data = await request.form()
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"
+        r"localhost|"
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+        r"(?::\d+)?"
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+    if not (re.match(regex, data["url"]) is not None):
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "error": "Invalid URL"}
+        )
     _hash = db.new_url(secrets.API_KEY, data["url"])
     return templates.TemplateResponse(
         "index.html", {"request": request, "short_url": str(request.base_url) + _hash}
--- shawty/api.py
+++ shawty/api.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 
 from fastapi import APIRouter
@@ -16,6 +17,17 @@ db = Database()
 
 @api.post("/new")
 async def new_url(*, api_key: str = Depends(auth_header), request: Request, url: str):
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"
+        r"localhost|"
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+        r"(?::\d+)?"
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+    if not (re.match(regex, url) is not None):
+        return {"message": "Invalid URL."}
     _hash = db.new_url(api_key, url)
     response = {"url": str(request.base_url) + _hash, "timestamp": datetime.utcnow()}
     return response
